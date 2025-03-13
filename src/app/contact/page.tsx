763b140a8a7fc260989cf444c2899882f997d77e
Sergey_js15 <serj.777.front@gmail.com>
2025-03-13 19:03:15 +0200

    Stylized components

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,9 +1,75 @@
-import { Header } from '../../components/Header';
+"use client"
 
+import { Header } from '../../components/Header';
+import { useForm, SubmitHandler } from "react-hook-form";
+import styles from '../../styles/pages/contact.module.scss';
+
+type TypeFormData = {
+    firstName: string,
+    lastName: string,
+    letter: string
+}
 const Contact = () => {
+    const { register, handleSubmit, formState: { errors } } = useForm<TypeFormData>();
+
+    const onSubmit: SubmitHandler<TypeFormData> = (data) => {
+        console.log(data)
+    }
+
     return (
-        <div>
+        <div className={styles['wrapper-contact']}>
             <Header title="Напишіть нам" />
+            <div className={styles['container-form']}>
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    <div className={styles['block-input']}>
+                        <label htmlFor="first-name">Ім'я:</label>
+                        <input
+                            id='first-name'
+                            autoComplete="off"
+                            {...register('firstName', {
+                                required: 'Це поле є обов\'язковим.',
+                                minLength: { value: 3, message: 'Ім\'я має бути мінімум 3 символи.' },
+                                pattern: {
+                                    value: /^[A-Za-zА-Яа-яЁёІіЇїЄєҐґ'ї]+$/i,
+                                    message: 'Ім\'я може містити тільки букви.'
+                                }
+                            })}
+                        />
+                    </div>
+                    <p className={styles.error}>{errors.firstName?.message}</p>
+                    <div className={styles['block-input']}>
+                        <label htmlFor="last-name">Прізвище:</label>
+                        <input
+                            id='last-name'
+                            autoComplete="off"
+                            {...register('lastName', {
+                                required: 'Це поле є обов\'язковим.',
+                                minLength: { value: 3, message: 'Прізвище має бути мінімум 3 символи.' },
+                                pattern: {
+                                    value: /^[A-Za-zА-Яа-яЁёІіЇїЄєҐґ'ї]+$/i,
+                                    message: 'Прізвище може містити тільки букви.'
+                                }
+                            })}
+                        />
+                    </div>
+                    <p className={styles.error}>{errors.lastName?.message}</p>
+                    <div className={styles['block-letter']}>
+                        <label htmlFor="letter">Повідомлення:</label>
+                        <textarea
+                            id='letter'
+                            autoComplete="off"
+                            {...register('letter', {
+                                required: 'Це поле є обов\'язковим.',
+                                minLength: { value: 10, message: 'Опишіть проблему або пропозицію детальніше.' }
+                            })}
+                        />
+                    </div>
+                    <p className={styles.error}>{errors.letter?.message}</p>
+                    <div className={styles['block-button']}>
+                        <button type="submit">Відправити</button>
+                    </div>
+                </form>
+            </div>
         </div>
     )
 };
--- src/app/tasks/page.tsx
+++ src/app/tasks/page.tsx
@@ -1,14 +1,17 @@
-import {Header} from '../../components/Header';
+import { Header } from '../../components/Header';
 import { GameCard } from '../../components/GameCard';
-import imageArithmetic from '../../assets/images/arithmetic/cloud.png'
+import styles from '../../styles/pages/tasks.module.scss';
+import imageArithmetic from '../../assets/images/arithmetic/cloud.png';
 
 const Task = () => {
-    return(
-        <div>
-          <Header title="Виберіть завдання:" />
-          <GameCard h5="Arithmetic" image={imageArithmetic} link="/games/arithmetic-trainer" />
-        </div>
-    )
+  return (
+    <div className={styles['tasks-container']}>
+      <Header title="Виберіть завдання:" />
+      <section className={styles['block-games']}>
+        <GameCard h5="Arithmetic" image={imageArithmetic} link="/games/arithmetic-trainer" />
+      </section>
+    </div>
+  )
 };
 
 export default Task;
